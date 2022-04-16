236820274f53311fe6707210968e81714585fe09
Tungnxu <tungnxu@outlook.com>
2022-04-16 22:58:08 +0700

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OurStoryComponent } from './sections/our-story/our-story.component';
 import { IntersectionObserverDirective } from './directive/intersection-observer.directive';
@@ -18,7 +17,7 @@ import { GalleryComponent } from './sections/gallery/gallery.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
