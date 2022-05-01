6c3088664a6bdd1f60b0f9873ec73fe5d7589f44
Tungnxu <tungnxu@outlook.com>
2022-05-01 14:24:38 +0700

    update

--- src/app/sections/love-gift/love-gift.component.ts
+++ src/app/sections/love-gift/love-gift.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoveGiftComponent implements OnInit {
 
+  isShow: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showGift(){
+    this.isShow = true;
   }
 
 }
